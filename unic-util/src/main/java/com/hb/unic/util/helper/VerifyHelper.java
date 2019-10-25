fdce2356e4b4b51c6d43168c4d8c1b557f91807f
黄彪 <191059014@qq.com>
2019-10-25 23:32:23 +0800

    修复重置密码的问题

--- unic-util/src/main/java/com/hb/unic/util/helper/VerifyHelper.java
+++ unic-util/src/main/java/com/hb/unic/util/helper/VerifyHelper.java
@@ -24,7 +24,7 @@ public class VerifyHelper {
         if (StringUtils.isBlank(mobile) || mobile.length() != 11) {
             return false;
         }
-        String regex = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
+        String regex = "^1([3|4|5|6|7|8|9])\\d{9}$";
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(mobile);
         boolean isMatch = m.matches();
