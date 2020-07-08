70b24a253ff406e158fe59563fe0d931f0012291
黄彪 <191059014@qq.com>
2020-07-08 10:04:19 +0800

    优化代码

--- unic-util/src/test/java/com/hb/unic/util/Base64EncryptTest.java
+++ unic-util/src/test/java/com/hb/unic/util/Base64EncryptTest.java
@@ -1,5 +1,6 @@
 package com.hb.unic.util;
 
+import com.hb.unic.util.constant.UtilConsts;
 import com.hb.unic.util.tool.Base64Encrypt;
 import org.junit.Test;
 
@@ -15,20 +16,20 @@ public class Base64EncryptTest {
 
     @Test
     public void testEncode() {
-        System.out.println(Base64Encrypt.JDK.encode(message, ));
-        System.out.println(Base64Encrypt.COMMONS_CODEC.encode(message, ));
+        System.out.println(Base64Encrypt.JDK.encode(message, UtilConsts.ISO_8859_1));
+        System.out.println(Base64Encrypt.COMMONS_CODEC.encode(message, UtilConsts.ISO_8859_1));
     }
 
     @Test
     public void testDecode() {
-        System.out.println(Base64Encrypt.JDK.decode("MTIzNDU25oKo5aW9PT0rK0Am", ));
-        System.out.println(Base64Encrypt.COMMONS_CODEC.decode("MTIzNDU25oKo5aW9PT0rK0Am", ));
+        System.out.println(Base64Encrypt.JDK.decode("MTIzNDU25oKo5aW9PT0rK0Am", UtilConsts.ISO_8859_1));
+        System.out.println(Base64Encrypt.COMMONS_CODEC.decode("MTIzNDU25oKo5aW9PT0rK0Am", UtilConsts.ISO_8859_1));
     }
 
     @Test
     public void testVerify() {
-        System.out.println(Base64Encrypt.JDK.verify(message, "MTIzNDU25oKo5aW9PT0rK0Am", ));
-        System.out.println(Base64Encrypt.COMMONS_CODEC.verify(message, "MTIzNDU25oKo5aW9PT0rK0Am", ));
+        System.out.println(Base64Encrypt.JDK.verify(message, "MTIzNDU25oKo5aW9PT0rK0Am", UtilConsts.ISO_8859_1));
+        System.out.println(Base64Encrypt.COMMONS_CODEC.verify(message, "MTIzNDU25oKo5aW9PT0rK0Am", UtilConsts.ISO_8859_1));
     }
 
 }
--- unic-util/src/test/java/com/hb/unic/util/SingleTrackEncryptTest.java
+++ unic-util/src/test/java/com/hb/unic/util/SingleTrackEncryptTest.java
@@ -1,5 +1,6 @@
 package com.hb.unic.util;
 
+import com.hb.unic.util.constant.UtilConsts;
 import com.hb.unic.util.tool.SingleTrackEncrypt;
 import org.junit.Test;
 
@@ -15,19 +16,19 @@ public class SingleTrackEncryptTest {
 
     @Test
     public void testEncode() {
-        String md5_encode = SingleTrackEncrypt.MD5.encode(message, );
+        String md5_encode = SingleTrackEncrypt.MD5.encode(message, UtilConsts.ISO_8859_1);
         System.out.println(md5_encode + "\t长度：" + md5_encode.length());
-        String sha_1_encode = SingleTrackEncrypt.SHA_1.encode(message, );
+        String sha_1_encode = SingleTrackEncrypt.SHA_1.encode(message, UtilConsts.ISO_8859_1);
         System.out.println(sha_1_encode + "\t长度：" + sha_1_encode.length());
-        String sha_256_encode = SingleTrackEncrypt.SHA_256.encode(message, );
+        String sha_256_encode = SingleTrackEncrypt.SHA_256.encode(message, UtilConsts.ISO_8859_1);
         System.out.println(sha_256_encode + "\t长度：" + sha_256_encode.length());
     }
 
     @Test
     public void testVerify() {
-        System.out.println(SingleTrackEncrypt.MD5.verify(message, "e3eccac1919ca38f864037c8d171967c", ));
-        System.out.println(SingleTrackEncrypt.SHA_1.verify(message, "c9a2ef34cf097ecd828bc57f0a3fc755448c2b5e", ));
-        System.out.println(SingleTrackEncrypt.SHA_256.verify(message, "82f37f2da56f00fd70c667263b05ca52e661dce419d73d4a9ad5a1685e6a6be8", ));
+        System.out.println(SingleTrackEncrypt.MD5.verify(message, "e3eccac1919ca38f864037c8d171967c", UtilConsts.ISO_8859_1));
+        System.out.println(SingleTrackEncrypt.SHA_1.verify(message, "c9a2ef34cf097ecd828bc57f0a3fc755448c2b5e", UtilConsts.ISO_8859_1));
+        System.out.println(SingleTrackEncrypt.SHA_256.verify(message, "82f37f2da56f00fd70c667263b05ca52e661dce419d73d4a9ad5a1685e6a6be8", UtilConsts.ISO_8859_1));
     }
 
 }
