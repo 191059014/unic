541ad5e692fe2223fe4ad50ddf656e160a3d297a
黄彪 <191059014@qq.com>
2020-07-24 13:20:12 +0800

    优化日志

--- unic-base/src/main/java/com/hb/unic/base/GlobalProperties.java
+++ unic-base/src/main/java/com/hb/unic/base/GlobalProperties.java
@@ -1,7 +1,7 @@
 package com.hb.unic.base;
 
-import com.hb.unic.logger.Logger;
-import com.hb.unic.logger.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
