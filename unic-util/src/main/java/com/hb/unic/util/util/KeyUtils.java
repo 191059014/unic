52f308c51fea5c3f46af98fc4db797836231c776
guoll <guoll@19pay.com.cn>
2020-09-05 16:51:01 +0800

    代码优化

--- unic-util/src/main/java/com/hb/unic/util/util/KeyUtils.java
+++ unic-util/src/main/java/com/hb/unic/util/util/KeyUtils.java
@@ -11,7 +11,7 @@ public class KeyUtils {
      * 生成唯一key
      *
      * @param pkFlag key标识
-     * @return 标识+yyyyMMddHHmmssSSS（17位）+随机数（8位）
+     * @return 标识+yyyyMMddHHmmssSSS（17位）+随机数（6位）
      */
     public static String getUniqueKey(String pkFlag) {
         return new StringBuilder(pkFlag).append(DateUtils.getNowTimeStr(DateUtils.FORMAT_8)).append(RandomUtils.getRandomOfLength(6)).toString();
