5e8336c547af58c3472a91d353424163b6ccee18
guoll <guoll@19pay.com.cn>
2020-09-12 09:02:49 +0800

    代码优化

--- unic-util/src/main/java/com/hb/unic/util/util/KeyUtils.java
+++ unic-util/src/main/java/com/hb/unic/util/util/KeyUtils.java
@@ -11,10 +11,19 @@ public class KeyUtils {
      * 生成唯一key
      *
      * @param pkFlag key标识
-     * @return 标识+yyyyMMddHHmmssSSS（17位）+随机数（6位）
+     * @return 标识+yyMMddHHmmssSSS（15位）+随机数（6位）
      */
     public static String getUniqueKey(String pkFlag) {
         return new StringBuilder(pkFlag).append(DateUtils.getNowTimeStr(DateUtils.FORMAT_12)).append(RandomUtils.getRandomOfLength(6)).toString();
+    }
+
+    /**
+     * 获取多租户ID
+     *
+     * @return yyMMddHHmmssSSS（15位）
+     */
+    public static String getTenantId() {
+        return DateUtils.getNowTimeStr(DateUtils.FORMAT_12);
     }
 
 }
